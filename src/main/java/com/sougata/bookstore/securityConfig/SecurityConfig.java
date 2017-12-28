3235db556cbedca612358ca21ea188dbe3de1af4
SOUGATA ROY <sougata.rintu@gmail.com>
2017-12-28 11:07:25 +0530

    security and users added

--- src/main/java/com/sougata/bookstore/securityConfig/SecurityConfig.java
+++ src/main/java/com/sougata/bookstore/securityConfig/SecurityConfig.java
@@ -3,21 +3,28 @@ package com.sougata.bookstore.securityConfig;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("").password("123").roles("admin");
+        auth.inMemoryAuthentication().withUser("sougata").password("1234").roles("admin");
+
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
-                .anyRequest().authenticated()
+        http.httpBasic()
                 .and()
-                .logout().permitAll();
+                    .authorizeRequests()
+                        .antMatchers("/api/users/**").hasRole("admin")
+                        .antMatchers("/api/books/**").hasRole("admmin")
+                        .antMatchers("/api/books/").permitAll()
+                        .antMatchers("/api/books/save").hasRole("manager")
+                .and()
+                .csrf().disable().headers().frameOptions().disable();
 
     }
 }
